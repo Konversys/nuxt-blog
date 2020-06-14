416d9097cc6aca2720912f75fc586175c8334333
Халилов Рустам <konversys@gmail.com>
2020-06-14 19:58:46 +0700

    урок 20

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
-  <div></div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <AppPost v-for="post in 3" :key="post" />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import AppPost from "@/components/main/Post";
 export default {
-  components: {}
+  components: {
+    AppPost
+  },
+  head: {
+    title: "Главная"
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+
 </style>
