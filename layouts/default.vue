96e3bdacc7725e4c37f7de6019a5509443b38a7b
Халилов Рустам <konversys@gmail.com>
2020-06-14 15:24:00 +0700

    урок 19

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <el-container>
+    <el-header>
+      <AppNavigation />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
 </template>
 
-<style>
+<script>
+import AppNavigation from "@/components/main/Navigation";
 
+export default {
+  components: {
+    AppNavigation
+  }
+};
+</script>
+
+<style>
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,18 @@ module.exports = {
         content: process.env.npm_package_description || ""
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,300&display=swap"
+      }
+    ]
   },
   loading: { color: "#fff" },
 
-  css: ["element-ui/lib/theme-chalk/index.css"],
+  css: ["element-ui/lib/theme-chalk/index.css", "@/theme/index.scss"],
 
   plugins: ["@/plugins/globals"],
 
