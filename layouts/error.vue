3a0df153bf05b0663da0bd6f786621a1d95c17c5
Халилов Рустам <konversys@gmail.com>
2020-06-14 22:36:24 +0700

    урок 23

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="error-layout">
+    <div class="error-content">
+      <h1 class="error-title">Страница не найдена</h1>
+      <nuxt-link to="/">Вернуться на главную</nuxt-link>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.error-layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.error-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.error-title {
+  color: #f00505;
+  margin-bottom: 1rem;
+}
+</style>
