81321806eaeb780269046fc1790cbc526721eb6e
Халилов Рустам <konversys@gmail.com>
2020-06-16 01:50:42 +0700

    урок 28

---
+++ layouts/admin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <el-container :style="{ height: '100%' }">
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <nuxt />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Admin page</div>
+</template>
+
+<script>
+export default {
+  layout: "admin"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
