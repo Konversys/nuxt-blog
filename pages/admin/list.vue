51c6aac8149d77548ab4a60a6b7cfa78b604b3fd
Халилов Рустам <konversys@gmail.com>
2020-06-22 00:33:46 +0700

    36

--- pages/admin/list.vue
+++ pages/admin/list.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>posts</div>
+  <el-table :data="posts" style="width: 100%">
+    <el-table-column prop="title" label="Заголовок" />
+    <el-table-column label="Дата">
+      <template slot-scope="{row: {date}}">
+        <i class="el-icon-time"></i>
+        <span>{{ new Date(date).toLocaleString() }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Просмотры">
+      <template slot-scope="{row: {views}}">
+        <i class="el-icon-message"></i>
+        <span>{{ views }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Комментарии">
+      <template slot-scope="{ row: {comments}}">
+        <i class="el-icon-chat-line-square"></i>
+        <span>{{ comments.length }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Операции">
+      <template slot-scope="{row}">
+        <el-button size="mini" circle type="primary" icon="el-icon-edit" @click="open(row._id)"/>
+        <el-button size="mini" circle type="danger" icon="el-icon-delete" @click="remove(row._id)"/>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
   layout: "admin",
-  middleware: ['admin-auth']
+  middleware: ["admin-auth"],
+  async asyncData({ store }) {
+    const posts = await store.dispatch("post/fetchAdmin");
+    return {
+      posts
+    };
+  },
+  methods: {
+    open(id) {
+      console.log('open', id);
+      
+    },
+    remove(id) {
+      console.log('remove', id);
+    }
+  }
 };
 </script>
 
---
+++ store/post.js
@@ -0,0 +1,24 @@
+export const actions = {
+  async fetchAdmin({}) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          {
+            title: "post",
+            date: new Date(),
+            views: 22,
+            comments: [1, 2],
+            _id: Math.random()
+          },
+          {
+            title: "post2",
+            date: new Date(),
+            views: 23,
+            comments: [1, 2],
+            _id: Math.random()
+          }
+        ]);
+      }, 1000);
+    });
+  }
+};
