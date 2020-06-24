572c3dc38f8bdb868530e36f6f6095a83b514597
Халилов Рустам <konversys@gmail.com>
2020-06-25 03:15:39 +0700

    урок 44

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const keys = require("./keys");
 const app = express();
+
+mongoose
+  .connect(keys.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongo Connected"))
+  .catch(e => console.log(e));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    
-}
+  MONGO_URI: `mongodb+srv://konver:1111@cluster0-6mdbo.mongodb.net/nuxt-blog?retryWrites=true&w=majority`
+};
