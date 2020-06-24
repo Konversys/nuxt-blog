d2f1c4e67660116e38f4ae2dcd7f0e606bce4eea
Халилов Рустам <konversys@gmail.com>
2020-06-25 03:23:19 +0700

    урок 45

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth.routes");
 const keys = require("./keys");
 const app = express();
 
@@ -11,5 +12,7 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
