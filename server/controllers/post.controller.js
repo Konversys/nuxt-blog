bcf029349799a7d2786000871100f8bf3e118dc3
Халилов Рустам <konversys@gmail.com>
2020-06-29 00:21:31 +0700

    урок 52

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,13 +1,76 @@
 const Post = require("../models/user.model");
 
-module.exports.create = async (req, res) => {};
+module.exports.create = async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    text: req.body.text,
+    imageUrl: `/${req.body.filename}`
+  });
 
-module.exports.getAll = async (req, res) => {};
+  try {
+    await post.save();
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-module.exports.getById = async (req, res) => {};
+module.exports.getAll = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ data: -1 });
+    req.json(posts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-module.exports.update = async (req, res) => {};
+module.exports.getById = async (req, res) => {
+  try {
+    await Post.findById(req.params.id)
+      .populate("comments")
+      .exec((error, post) => {
+        res.json(post);
+      });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-module.exports.remove = async (req, res) => {};
+module.exports.update = async (req, res) => {
+  const $set = {
+    text: req.body.text
+  };
 
-module.exports.addView = async (req, res) => {};
+  try {
+    const post = await Post.findByIdAndUpdate(
+      {
+        _id: req.params.id
+      },
+      { $set }
+    );
+    res.json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports.remove = async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.json({ message: "Пост удален" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports.addView = async (req, res) => {
+  const $set = {
+    views: ++req.body.views
+  };
+  try {
+    await Post.findOneAndUpdate({ _id: req.params.id }, { $set });
+    res.status(204).json();
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
