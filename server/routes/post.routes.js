d50cf3074a494ae114aa945bae25e9fed9ee88ff
Халилов Рустам <konversys@gmail.com>
2020-06-28 23:36:37 +0700

    урок 50

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,5 +1,41 @@
 const passport = require("passport");
 const { Router } = require("express");
+const controller = require("../controllers/post.controller");
 const router = Router();
+
+// /api/post
+router.post(
+  "/admin/",
+  passport.authenticate("jwt", { session: false }),
+  controller.create
+);
+
+router.get(
+  "/admin/",
+  passport.authenticate("jwt", { session: false }),
+  controller.getAll
+);
+
+router.get(
+  "/admin/:id",
+  passport.authenticate("jwt", { session: false }),
+  controller.getById
+);
+
+router.put(
+  "/admin/:id",
+  passport.authenticate("jwt", { session: false }),
+  controller.update
+);
+
+router.delete(
+  "/admin/:id",
+  passport.authenticate("jwt", { session: false }),
+  controller.remove
+);
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+router.put('/:id', controller.addView)
 
 module.exports = router;
