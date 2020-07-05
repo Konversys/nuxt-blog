e426be12c3ff3fdcdb2e88ec6bd91ddeef4679a5
Халилов Рустам <konversys@gmail.com>
2020-07-05 23:11:34 +0700

    урок 59

--- store/auth.js
+++ store/auth.js
@@ -1,3 +1,7 @@
+import Cookie from "cookie";
+import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+
 export const state = () => ({
   token: null
 });
@@ -27,10 +31,12 @@ export const actions = {
   setToken({ commit }, token) {
     this.$axios.setToken(token, "Bearer");
     commit("setToken", token);
+    Cookies.set("token", token);
   },
   logout({ commit }) {
     this.$axios.setToken(false);
     commit("clearToken");
+    Cookies.remove("token");
   },
   async createUser({ commit }, formData) {
     try {
@@ -38,10 +44,32 @@ export const actions = {
     } catch (error) {
       commit("setError", error, { root: true });
     }
+  },
+  autoLogin({ dispatch }) {
+    const cookieStr = process.browser
+      ? document.cookie
+      : this.app.context.req.headers.cookie;
+
+    const cookies = Cookie.parse(cookieStr || "") || {};
+    const token = cookies["token"];
+
+    if (isJWTValid(token)) {
+      dispatch("setToken", token);
+    } else {
+      dispatch("logout");
+    }
   }
 };
 
 export const getters = {
   isAuthenticated: state => !!state.token,
   token: state => state.token
+};
+
+const isJWTValid = token => {
+  if (!token) return false;
+
+  const jwtData = jwtDecode(token) || {};
+  const expires = jwtData.exp || 0;
+  return new Date().getTime() / 1000 < expires;
 };
